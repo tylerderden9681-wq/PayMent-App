6155aee9903b39c6ff5c50c5948d552abb86cef2
Karim <tylerderden9681@gmail.com>
2026-03-23 21:32:49 +0300

    1.0.1: Добавлены разделения ручек на создание кошелька и на пополнение баланса, а также реализовано деление операций на доход и расход, но не учитывается принцип DRY

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 
 # Создаем приложение на FastAPI (Создаем экземпляр класса):
@@ -8,6 +9,13 @@ app = FastAPI()
 # Ключ - название кошелька, значение - баланс
 
 BALANCE = {'apple': 100}
+
+
+# Объявим модель Pydantic для разделения операций доход и расхода:
+class Operation(BaseModel):
+    wallet_name: str
+    amount: float
+    description: str | None = None
 
 # Здесь мы указываем body-запросы, то есть, если мы заходим написать это в браузере,
 # то это будем выглядеть так: localhost:8000/balance?wallet_name=...
@@ -29,21 +37,69 @@ def get_balance(wallet_name: str | None = None):
 
 # Так как мы уже написала body-запросы, самое время - написать ту же самую ручку с помощью path-запроса:
 @app.post('/wallets/{name}')
-def recieve_money(name: str, amount: int):
-    # Если кошелька с таким именем нет, то создаем новый кошелёк с таким именем и с балансом 0
-    if name not in BALANCE:
-        BALANCE[name] = 0
-    # Если есть - добавляем amount к балансу указанного кошелька
-    BALANCE[name] += amount
-    # И обязательно возвращаем информацию о завершении операции
-    return {
-        'ok': True,
-        'msg': 'Транзакция прошла успешно!',
-        'details': {
-            'detail': f'Added {amount} to {name}',
-            'wallet': name,
-            'past_balance': BALANCE[name] - amount,
-            'new_balance': BALANCE[name]
-        }
-    }
+def create_wallet(name: str, amount: float = 0):
+    """Суть такая:
+    1) Будем проверять, существует ли уже такой кошелёк
+    1.1) Если да, то возвращаем код 400 (что-то пошло не так) с пояснением
+    1.2) Если нет, то создаем его с переданным балансом
+    2) В конце концов, возвращаем, что создание кошелька прошло успешно"""
+
+    if name in BALANCE:
+        raise HTTPException(status_code=400,
+                            detail='Такой кошелёк уже есть')
     
+    BALANCE[name] = amount
+
+    return {'ok': True,
+            'details': {
+                'msg': 'Операция создания кошелька прошла успешно!',
+                'balance': f'Текущий баланс на вашем счёте - {BALANCE[name]}'
+            }}
+    
+
+# Сделаем 2 ручки: доход и расход
+
+# 1) Доход:
+@app.post('/operations/income')
+def income_money(operation: Operation):
+    """Логика такая:
+    1) Проверяем, существует ли кошелёк
+    2) Проверяем, что сумма положительная
+    3) Добавляем доход к балансу кошелька
+    4) Возвращаем сообщение об успешной транзакции"""
+
+    if operation.wallet_name not in BALANCE:
+        raise HTTPException(status_code=404, detail=f'Wallet {operation.wallet_name} not found in DB')
+    
+    if operation.amount <= 0:
+        raise HTTPException(status_code=400, detail='Balance is must be positive')
+    
+    BALANCE[operation.wallet_name] += operation.amount
+
+    return {'ok': True,
+            'msg': 'The transaction was successful'}
+
+
+# 2) Расход:
+@app.post('/operations/expense')
+def expense_money(operation: Operation):
+    """Логика такая:
+    1) Проверяем, существует ли кошелёк
+    2) Проверяем, что сумма положительная (вычитаемое)
+    3) Проверяем, достаточно ли средств в кошельке
+    4) Убавляем переданную сумму из баланса кошелька
+    5) Возвращаем сообщение об успешной транзакции"""
+
+    if operation.wallet_name not in BALANCE:
+        raise HTTPException(status_code=404, detail=f'Wallet {operation.wallet_name} is not defined')
+    
+    if operation.amount <= 0:
+        raise HTTPException(status_code=400, detail=f'Balance is must be positive')
+    
+    if BALANCE[operation.wallet_name] < operation.amount:
+        raise HTTPException(status_code=400, 
+                            detail=f'Недостаточно средств. Доступные деньги: {BALANCE[operation.wallet_name]}')
+    
+    BALANCE[operation.wallet_name] -= operation.amount
+
+    return {'ok': True, 'msg': 'The traksaktion is successfully'}
