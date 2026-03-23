0ffbba57b3d989ec7a5bff5571089dbd0e02c4d6
Karim <tylerderden9681@gmail.com>
2026-03-23 22:07:51 +0300

    Добавлена валидация данных с помощью Pydantic и теперь код не повторяется в двух местах, а именно проверка на неотрицательность

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, field_validator
 
 
 # Создаем приложение на FastAPI (Создаем экземпляр класса):
@@ -12,10 +12,33 @@ BALANCE = {'apple': 100}
 
 
 # Объявим модель Pydantic для разделения операций доход и расхода:
-class Operation(BaseModel):
-    wallet_name: str
+class OperationSchema(BaseModel):
+    # Ограничим ввод некоторых данных с помощью валидации данных в Pydantic:
+    wallet_name: str = Field(..., max_length=127)
     amount: float
-    description: str | None = None
+    description: str | None = Field(None,  max_length=255)
+
+    # Чтобы ограничить поле amount используем функцию field_validator как декоратор:
+    # Это валидатор, что сумма положительная
+    @field_validator('amount')
+    def amount_must_be_positive(cls, value: float) -> float:
+        # Логика такая: нам нужно проверить, что amount >= 0 и возвратить значение если всё ок
+        if value < 0:
+            raise HTTPException(status_code=400, detail='Amount must be positive or null')
+        
+        return value
+    
+    # Нам также важно, чтобы пользователь не передавал только пробелы, иначе это будет некорректно
+    # Так что обработаем и это с помощью валидатора:
+    @field_validator('wallet_name')
+    def wallet_name_is_not_empty(cls, name: str) -> str:
+        # Убираем пробелы по краям с помощью .strip() и проверяем, не пуста ли строка:
+        clean_str = name.strip()
+
+        if not clean_str:
+            raise HTTPException(status_code=400, detail='У кошелька должно быть свое имя, а не пробелы!!!')
+
+    
 
 # Здесь мы указываем body-запросы, то есть, если мы заходим написать это в браузере,
 # то это будем выглядеть так: localhost:8000/balance?wallet_name=...
@@ -35,25 +58,50 @@ def get_balance(wallet_name: str | None = None):
     return {'wallet': wallet_name, 'balance': BALANCE[wallet_name]}
 
 
+class CreateWalletSchema(BaseModel):
+    name: str = Field(..., max_length=127)
+    initial_balance: float
+
+    # Чтобы ограничить поле amount используем функцию field_validator как декоратор:
+    # Нам также важно, чтобы пользователь не передавал только пробелы, иначе это будет некорректно
+    # Так что обработаем и это с помощью валидатора:
+    @field_validator('name')
+    def wallet_name_is_not_empty(cls, name: str) -> str:
+        # Убираем пробелы по краям с помощью .strip() и проверяем, не пуста ли строка:
+        clean_str = name.strip()
+
+        if not clean_str:
+            raise HTTPException(status_code=400, detail='У кошелька должно быть свое имя, а не пробелы!!!')
+
+    # Это валидатор, что сумма положительная
+    @field_validator('initial_balance')
+    def amount_must_be_positive(cls, value: float) -> float:
+        # Логика такая: нам нужно проверить, что amount >= 0 и возвратить значение если всё ок
+        if value < 0:
+            raise ValueError("Initial balanse shouldn't be negative")
+        
+        return value
+
+
 # Так как мы уже написала body-запросы, самое время - написать ту же самую ручку с помощью path-запроса:
-@app.post('/wallets/{name}')
-def create_wallet(name: str, amount: float = 0):
+@app.post('/wallets')
+def create_wallet(wallet: CreateWalletSchema):
     """Суть такая:
     1) Будем проверять, существует ли уже такой кошелёк
     1.1) Если да, то возвращаем код 400 (что-то пошло не так) с пояснением
     1.2) Если нет, то создаем его с переданным балансом
     2) В конце концов, возвращаем, что создание кошелька прошло успешно"""
 
-    if name in BALANCE:
+    if wallet.name in BALANCE:
         raise HTTPException(status_code=400,
                             detail='Такой кошелёк уже есть')
     
-    BALANCE[name] = amount
+    BALANCE[wallet.name] = wallet.initial_balance
 
     return {'ok': True,
             'details': {
                 'msg': 'Операция создания кошелька прошла успешно!',
-                'balance': f'Текущий баланс на вашем счёте - {BALANCE[name]}'
+                'balance': f'Текущий баланс на вашем счёте - {BALANCE[wallet.name]}'
             }}
     
 
@@ -61,7 +109,7 @@ def create_wallet(name: str, amount: float = 0):
 
 # 1) Доход:
 @app.post('/operations/income')
-def income_money(operation: Operation):
+def income_money(operation: OperationSchema):
     """Логика такая:
     1) Проверяем, существует ли кошелёк
     2) Проверяем, что сумма положительная
@@ -71,9 +119,6 @@ def income_money(operation: Operation):
     if operation.wallet_name not in BALANCE:
         raise HTTPException(status_code=404, detail=f'Wallet {operation.wallet_name} not found in DB')
     
-    if operation.amount <= 0:
-        raise HTTPException(status_code=400, detail='Balance is must be positive')
-    
     BALANCE[operation.wallet_name] += operation.amount
 
     return {'ok': True,
@@ -82,7 +127,7 @@ def income_money(operation: Operation):
 
 # 2) Расход:
 @app.post('/operations/expense')
-def expense_money(operation: Operation):
+def expense_money(operation: OperationSchema):
     """Логика такая:
     1) Проверяем, существует ли кошелёк
     2) Проверяем, что сумма положительная (вычитаемое)
@@ -92,9 +137,6 @@ def expense_money(operation: Operation):
 
     if operation.wallet_name not in BALANCE:
         raise HTTPException(status_code=404, detail=f'Wallet {operation.wallet_name} is not defined')
-    
-    if operation.amount <= 0:
-        raise HTTPException(status_code=400, detail=f'Balance is must be positive')
     
     if BALANCE[operation.wallet_name] < operation.amount:
         raise HTTPException(status_code=400, 
